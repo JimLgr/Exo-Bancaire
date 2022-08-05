79010254c5d8e2e1a5c4f57b96708abe215d6040
Jim Legrandhenri <jimlegrandhenri007@outlook.com>
2022-08-05 14:40:12 +0200

    exo bancaire

---
+++ ConsoleUsersInterface/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUsersInterface
+{
+    public class BankAccount
+    {
+        private static long m_LastNumber = 121;
+        //Les données ==> état de l'objet
+        private long m_accountNumber;
+        private decimal m_balance;
+        public string m_nameOwner;
+        public AccountBankTypes Type;
+       
+        public long AccountNumber
+        {
+            get => m_accountNumber;
+            private set => m_accountNumber = value;
+        }
+        
+
+        public BankAccount(string pNameOwner, AccountBankTypes pType)
+        {
+            m_nameOwner = pNameOwner;
+            Type = pType;
+            m_balance = 0;
+            this.m_accountNumber = ++m_LastNumber;
+        }
+
+
+
+        //Les méthodes ==> Comportement de l'objet
+        public decimal Deposit(decimal pAmount)
+        {
+            m_balance += pAmount;
+            return m_balance;
+        }
+        public bool Whithdraw(decimal pAmount)
+        {
+            
+            if (m_balance < pAmount) return false;
+            m_balance -= pAmount;
+            return true;
+        }
+        public decimal GetBalance()
+        {
+            return m_balance;
+        }
+        public override string ToString()
+        {
+            return $"{this.AccountNumber} => {this.m_nameOwner} => {this.Type} => {this.m_balance}";
+        }
+    }
+}
---
+++ ConsoleUsersInterface/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleUsersInterface;
+using System.Text.RegularExpressions;
+
+BankAccount jim = new BankAccount("Jim", AccountBankTypes.courant);
+Console.WriteLine($"{jim.AccountNumber} => {jim.m_nameOwner} => {jim.Type} => {jim.GetBalance()}");
+jim.Deposit(1000);
+Console.WriteLine($"{jim.m_nameOwner} {jim.Type} {jim.GetBalance()}");
+jim.Whithdraw(700);
+Console.WriteLine($"{jim.m_nameOwner} {jim.Type} {jim.GetBalance()}");
+bool resultat = jim.Whithdraw(500);
+Console.WriteLine(resultat);
+Console.WriteLine($"{jim}");
+
+
+BankAccount gomez = new BankAccount("Gomez", AccountBankTypes.epargne);
+Console.WriteLine($"{gomez}" );
+
+
+BankAccount troisieme = new("Leskro", AccountBankTypes.titre);
+Console.WriteLine($"{troisieme}");
+Console.ReadLine();
+
+
+
+
+
+//Console.WriteLine("Prenom ?");
+//string prenom = Console.ReadLine();
+//Console.WriteLine("Nom?");
+//string nom = Console.ReadLine();
+//Console.WriteLine("Votre date de naissance ?");
+//int.TryParse(Console.ReadLine(), out int inDate);
+//DateTime dateDeNaissance = DateTime.Parse(Console.ReadLine());
+//Console.WriteLine("Quel type de compte voulez vous ouvrir ?");
+//string type = Console.ReadLine().ToLower();
+
